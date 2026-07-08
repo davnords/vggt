486d941cc681dcbd11d1037548833c62b48bea4b
David Nordström <davnords@arrhenius1.hpc.arrhenius.naiss.se>
2026-07-08 11:18:23 +0200

    improve usability

--- test.sh
+++
@@ -1,6 +0,0 @@
-find ./data/re10k -type d -name images | while read imgdir; do
-  count=$(find "$imgdir" -maxdepth 1 -type f | wc -l)
-  if [ "$count" -gt 10 ]; then
-    basename "$(dirname "$imgdir")"
-  fi
-done > folders_with_many_images.txt
